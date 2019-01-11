97c6405b45494bb36f42703bece472806cbe3677
Eddie Colon <coloed3@gmail.com>
2019-01-10 22:38:42 -0700

    documenting the requires and started gathering pieces for the queries

--- query.py
+++ query.py
@@ -1,1 +1,14 @@
-import 
+import  psycopg2
+
+"""
+3 queries
+
+1. What are the most popular three articles of all time?
+2. Who are the most popular article authors of all time ?
+3. On Which days did more than 1% of the request lead to errors
+
+
+
+
+
+"""
