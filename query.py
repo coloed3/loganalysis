ed83d58aa9ce0c495cffc3f9dcdd1742296aef32
Eddie Colon <coloed3@gmail.com>
2019-01-10 23:02:44 -0700

    Created class to connect to the news database, added pprint for formatting

--- query.py
+++ query.py
@@ -1,4 +1,6 @@
-import  psycopg2
+import  psycopg2 # needed to create connection to database
+import pprint #https://docs.python.org/3/library/pprint.html allows to format the object we are printing
+
 
 """
 3 queries
@@ -7,8 +9,19 @@ import  psycopg2
 2. Who are the most popular article authors of all time ?
 3. On Which days did more than 1% of the request lead to errors
 
-
-
-
-
+(think about adding views to the queries)
 """
+
+# following code reference pynative.com
+
+
+# using class to cnnect to database  using try/catch for errors abs
+
+class  DatabaseConnection:
+    def __init__(self):
+        try:
+            dbnews = 'news'
+            self.connection = psycopg2.connect(database=dbnews)
+            self.conenction.autocommit = True
+            self.cursor = self.connection.cursor()
+        except:
